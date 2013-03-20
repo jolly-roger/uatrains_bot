600db3fe3e38647ba3f8cd16ddf7638ce27c3ebd
roger <ionyshchenko@sequans.com>
2013-03-20 12:30:41 +0200

    i

--- __init__.py
+++ __init__.py
@@ -1,3 +1,36 @@
+import os
+import os.path
+import logging
+import logging.handlers
+
+
+LOGS_DIR = 'logs'
+
+os.chdir('/home/www')
+if not os.path.isdir(LOGS_DIR):
+    os.mkdir(LOGS_DIR)
+
+access_logger = logging.getLogger('werp_access')
+access_logger.setLevel(logging.DEBUG)
+access_logger_fh = logging.handlers.TimedRotatingFileHandler(LOGS_DIR + '/werp_access.log', when='midnight')
+access_logger_fh.setLevel(logging.DEBUG)
+access_logger_formatter = logging.Formatter('[%(asctime)s] %(message)s')
+access_logger_fh.setFormatter(access_logger_formatter)
+access_logger.addHandler(access_logger_fh)
+
+error_logger = logging.getLogger('werp_error')
+error_logger.setLevel(logging.DEBUG)
+error_logger_fh = logging.handlers.TimedRotatingFileHandler(LOGS_DIR + '/werp_error.log', when='midnight')
+error_logger_fh.setLevel(logging.DEBUG)
+error_logger_formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s %(message)s')
+error_logger_fh.setFormatter(error_logger_formatter)
+error_logger.addHandler(error_logger_fh)
+
 from . import spider
 
 app = spider.wsgi()
+
+
+
+
+error_logger.info('Yo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
