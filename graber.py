5ccf976650fa9bff44e56e8fc694efd41682b879
roger <ionyshchenko@sequans.com>
2013-03-20 11:53:09 +0200

    i

--- graber.py
+++ graber.py
@@ -6,7 +6,8 @@ from .uatrains.engine import drv
 
 
 def grab():
+    #ppool.map(drv.odessa.get_train_data, [tid for tid in range(0, 5000)])
     with multiprocessing.Pool(processes=32) as ppool:
-        #ppool.map(drv.odessa.get_train_data, [tid for tid in range(0, 5000)])
         ppool.map(drv.southwest.get_train_data, [tid for tid in range(0, 5000)])
+    with multiprocessing.Pool(processes=32) as ppool:    
         ppool.map(drv.passengers.get_train_data, [tid for tid in range(20000, 70000)])
